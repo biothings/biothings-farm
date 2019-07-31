32000c3193bfc975f8ce90cf7f76c49067a3d023
Sebastien Lelong <slelong@scripps.edu>
2019-07-31 11:34:27 -0700

    check user's registered api if matching requested one

--- aws/lambda/auth/function.py
+++ aws/lambda/auth/function.py
@@ -1,25 +1,12 @@
-from __future__ import print_function
+import boto3
 import json
 import logging
 import requests
 import time
+import re
 from jose import jwt, jwk
 from jose.utils import base64url_decode
-import boto3
-
-
-# def lambda_handler(event, context):
-#     # TODO implement
-#     print("from lambda_handler: %s %s" % (repr(event),repr(context)))
-#     logging.error("blabla")
-#     print("arf")
-#     # TODO: write code...
-#     return {
-#         'statusCode': 200,
-#         'body': json.dumps('Hello from Lambda seb!'),
-#         'event': json.dumps(event),
-#     }
-
+from arnparse import arnparse
 
 region = 'us-west-2'
 userpool_id = 'us-west-2_KAglRSC16'
@@ -30,9 +17,6 @@ keys_url = 'https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json'.forma
 # https://aws.amazon.com/blogs/compute/container-reuse-in-lambda/
 keys = requests.get(keys_url).json()['keys']
 cogidp = boto3.client('cognito-idp')
-user = cogidp.admin_get_user(UserPoolId=userpool_id,
-                                 Username="sebastien.lelong@gmail.com")
-print(user)
 
  
 def generatePolicy(principalId, effect, methodArn):
@@ -102,7 +86,27 @@ def is_user_has_permissions(username,methodArn):
     cogidp = boto3.client('cognito-idp')
     user = cogidp.admin_get_user(UserPoolId=userpool_id,
                                  Username=username)
-    print("user: %s" % user)
+    # extract registered api on user's profile
+    attrs = user["UserAttributes"]
+    btnames = [a["Value"] for a in attrs if a["Name"] == "custom:biothings-api-name"]
+    assert len(btnames) == 1, "No BioThings API listed in user attributes"
+    btnames = btnames.pop()
+    # it can be a comma separated list. comparison will be case-insensitive.
+    btnames = [b for b in map(str.lower,map(str.strip,btnames.split(",")))]
+    # user has to be confirmed
+    assert user.get("UserStatus") == "CONFIRMED", "User not confirmed"
+    # then the resource he's trying to access has to match his own biothings APIs
+    arn = arnparse(methodArn)
+    # arn resource iooks like "g4k1vo9uyi/beta/GET/farm/cgi/build_manager"
+    # we want to extract "cgi". Position after HTTP verb and then extract part after /farm
+    try:
+        path = re.match(r".*/[A-Z]+/farm/(.+)",arn.resource).groups()[0]
+        api_name = path.split("/")[0]
+        assert api_name in btnames, "User not allowed to access API %s (allowed APIs: %s)" % (repr(api_name),btnames)
+        print("Requested API '%s' in user's registered APIs %s" % (api_name,repr(btnames)))
+    except Exception as e:
+        print(e)
+        raise Exception("Invalid arn resource: %s" % repr(arn.resource))
  
 
 def lambda_handler(event, context):
@@ -114,20 +118,25 @@ def lambda_handler(event, context):
             token = event["headers"]['authorization']
         elif event.get("queryStringParameters",{}).get("auth"):
             token = event["queryStringParameters"]["auth"]
+        assert token, "No auth token found"
+        #print("token: %s" % token)
         claims = is_token_valid(token)
-        print(claims)
+        #print(claims)
         if claims:
+            print("Token valid")
             # Get principalId from idInformation
             principalId = claims['sub']
-            print("Token valid")
             is_user_has_permissions(principalId,event['methodArn'])
+            print("User has permissions")
             return generatePolicy(principalId, 'Allow', event['methodArn'])
 
         return generatePolicy(None, 'Deny', event['methodArn'])
  
     except Exception as err:
         # Deny access if the token is invalid
-        print(err)
+        print("General error: %s" % err)
+        #import logging
+        #logging.exception(err)
         return generatePolicy(None, 'Deny', event.get('methodArn',"arn:null"))
  
 
@@ -135,8 +144,10 @@ def lambda_handler(event, context):
 # AWS Lambda and any other local environments
 if __name__ == '__main__':
     # for testing locally you can enter the JWT ID Token here
-    #t = """eyJraWQiOiJpNGhTMDd2ZDBvQmowdGV5K05wTnVKQWd2VVhUVTF2Qm9tRVQ3dHVhRStFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyM2Q5MWI2Yi04YzFhLTRlMGUtYWQ4My0wOWIxMmMwNDg1MDgiLCJldmVudF9pZCI6IjgzZjI0NDExLTQwN2
-    t = """eyJraWQiOiJpNGhTMDd2ZDBvQmowdGV5K05wTnVKQWd2VVhUVTF2Qm9tRVQ3dHVhRStFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyM2Q5MWI2Yi04YzFhLTRlMGUtYWQ4My0wOWIxMmMwNDg1MDgiLCJldmVudF9pZCI6ImJlYWYyZmJjLTA4YzEtNDQ4OC1iZTg0LTA0OGY5MmI1MTY2YyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1NjQwODQyMTAsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX0tBZ2xSU0MxNiIsImV4cCI6MTU2NDA4NzgxMCwiaWF0IjoxNTY0MDg0MjEwLCJqdGkiOiI5MGNlMjE4Ny04Mzk2LTQ0ZjYtYmQ0MC0yNTIyZWJiZTFkNjQiLCJjbGllbnRfaWQiOiI2Njhxcjg3ZmM4NzVlbnFhZHZlN280Zm5vNyIsInVzZXJuYW1lIjoiMjNkOTFiNmItOGMxYS00ZTBlLWFkODMtMDliMTJjMDQ4NTA4In0.BBmZmVvjz00EfpbdCXHKmE-KVCwRkyQw4swjPmIpKgA-XPpeu-E8mtwDSEC5CPSSkGg0xggTAz7l1rf3Jcc4HZy1hJ5zRY1R0GnmIzGluFe2v8cHDI4cXLO65yCyfjaHYP0OAlVF3B78IgoEY2T3MSXv6Q10mD7_NZCF2W_WPSJOAmpsaBjlgmFHS-O7n2U5cWd9XlBeaqOa0O7vUeLrGEsQVb6ZBDbtw3yIUps2uQwlp8g6-ox4zPHCAaUinxHy2mp7p3sNXT5DDqgru4dqVwVFPJ6HQlEaZMtga5AXBY_4lC_oG-A3kLc--Wcn984dguWsGqP5ogjyuv2pLOt8KA"""
-    event = {'authorizationToken': t, "methodArn": "arn:test"}
+    t = """eyJraWQiOiJpNGhTMDd2ZDBvQmowdGV5K05wTnVKQWd2VVhUVTF2Qm9tRVQ3dHVhRStFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyM2Q5MWI2Yi04YzFhLTRlMGUtYWQ4My0wOWIxMmMwNDg1MDgiLCJldmVudF9pZCI6ImVhMWY2OTUxLTY4YjItNDBkYi1hZDJhLTJhNjNkNzZkNTNhMiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1NjQ1OTAxNzEsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy13ZXN0LTIuYW1hem9uYXdzLmNvbVwvdXMtd2VzdC0yX0tBZ2xSU0MxNiIsImV4cCI6MTU2NDU5Mzc3MSwiaWF0IjoxNTY0NTkwMTcxLCJqdGkiOiJjZWQ0NThkMS1hZGZjLTRlYTYtYTBkOS1kZjhkNjdkZDViMzgiLCJjbGllbnRfaWQiOiI2Njhxcjg3ZmM4NzVlbnFhZHZlN280Zm5vNyIsInVzZXJuYW1lIjoiMjNkOTFiNmItOGMxYS00ZTBlLWFkODMtMDliMTJjMDQ4NTA4In0.iUTpOnRMzXf51T2OELMVQALQBDKTfCOfa6lG_t5vIJUAdj9j7_Ww3ULbPUnzk-hOYRYVhJB7i5ydIyViRDurikQMqmXbZ7IOwbFR3hjGa0nGZvekO9M5VPSAall4EOC3GkVVEvsQ4Cx9l2qtKmOv5cPF1pOtCGcQotkDPhzSfy71rbtmB2awyvdbto_aYTWMx8zVbPq37D2EOcRkK-xBq1EONWAJhNo_AEdsKhN6QNDwatf8yAoxJ6cs1MJOZ7iRaJsgy7Mbuwm_EII0IdMyRn6Ndixsxj2puK9fRpzquRbYbrbjIwrFFNer3Cst8UZE6MEeyCalf_GicGvRfmQe_Q"""
+    arn = """arn:aws:execute-api:us-west-1:215751090072:g4k1vo9uyi/beta/GET/farm/cgi/build_manager"""
+    event = {"headers" : {'authorization': t}}
+    event["methodArn"] = arn
     policy = lambda_handler(event, None)
     print(policy)
+
